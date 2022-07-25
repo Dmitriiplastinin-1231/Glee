9b429ac419a04ae6acd41b33fd6101ae5a73ee43
Dmitriiplastinin-1231 <dimanplastinen@gmail.com>
2022-07-25 18:01:26 +0300

    added sections Product, Top and Design

--- app/js/main.js
+++ app/js/main.js
@@ -8,6 +8,16 @@ $(function() {
 		autoplaySpeed: 2000
 	})
 
+	
+	var containerEl1 = document.querySelector('[data-ref="filter"]');
+            var containerEl2 = document.querySelector('[data-ref="filter2"]');
 
+            var config = {
+                controls: {
+                    scope: 'local'
+                }
+            };
 
+            var mixer1 = mixitup(containerEl1, config);
+            var mixer1 = mixitup(containerEl2, config);
 });
