93419ab1aae5b2fd50d6e30ffca6757f74196f4f
Dmitriiplastinin-1231 <dimanplastinen@gmail.com>
2022-08-27 17:38:55 +0300

    added shop page

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,35 @@
 $(function() {
 	
+	$('.shop__button').on('click', function () {
+		$('.shop__button').removeClass('shop__button--active');
+		$(this).addClass('shop__button--active');
+	});
+
+	$('.shop__line').on('click', function () {
+		$('.product-item').addClass('product-item--line');
+	});
+
+	$('.shop__grid').on('click', function () {
+		$('.product-item').removeClass('product-item--line');
+	});
+
+	$(".star").rateYo({
+		starWidth: "18px",
+		fullStar: true,
+		ratedFill: "#ffcc00"
+	});
+
+
+	$(".filter-price__input").ionRangeSlider({
+		type: "double",
+		onChange: function (data) {
+    		$('.filter-price__from').text(data.from);
+    		$('.filter-price__to').text(data.to);
+        }
+	});
+
+
+
 	$('.top-slider__inner').slick({
 		dots:true,
 		arrows:false,
