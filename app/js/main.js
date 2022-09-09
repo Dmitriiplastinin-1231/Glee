48eff10091b3a977e4b4402a361e02d5d3181f0c
Dmitriiplastinin-1231 <dimanplastinen@gmail.com>
2022-09-09 10:09:40 +0300

    finalized page product

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,42 @@
 $(function() {
-	
+	$('.related__slide').slick({
+		slidesToShow: 4,
+  		slidesToScroll: 1,
+  		appendArrows:'.related__slide-arrows',
+  		prevArrow:'<img class="related__slide-arrow related__slide-prev" src="images/icons/arrow.svg" alt="arrow-left">',
+  		nextArrow:'<img class="related__slide-arrow related__slide-next" src="images/icons/arrow.svg" alt="arrow-right">'
+	})
+
+
+
+	$('.product-tabs__top-item').on('click', function(e){
+		e.preventDefault();
+		$('.product-tabs__content-item').removeClass('product-tabs__content-item--active');
+		$($(this).attr('href')).addClass('product-tabs__content-item--active');
+		$('.product-tabs__top-item').removeClass('product-tabs__top-item--active');
+		$(this).addClass('product-tabs__top-item--active');
+	});
+
+
+	$('.product-one__item-num').styler();
+
+
+
+	$('.product-slide__thumb').slick({
+		asNavFor: '.product-slide__big',
+  		focusOnSelect: true,
+		slidesToShow: 3,
+		slidesToScroll: 1,
+		vertical: true
+	});
+	$('.product-slide__big').slick({
+		asNavFor: '.product-slide__thumb',
+  		arrows: false,
+  		fade: true
+	});
+
+
+
 	$('.shop__button').on('click', function () {
 		$('.shop__button').removeClass('shop__button--active');
 		$(this).addClass('shop__button--active');
